caafa64bd36e19e737e5eb9ac5422e8288f9ce7f
46112 <duf325@naver.com>
2023-11-22 19:58:03 +0900

    test: apiKey 테스트2

--- tarot/src/main/java/com/tarotmate/tarot/domain/fortune/application/FortuneService.java
+++ tarot/src/main/java/com/tarotmate/tarot/domain/fortune/application/FortuneService.java
@@ -14,7 +14,7 @@ import org.springframework.web.client.RestTemplate;
 @RequiredArgsConstructor
 @Service
 public class FortuneService {
-    @Value("${openai.accessKey}")
+    @Value("${OPENAI_API_KEY}")
     private String apiKey;
 
     private final RestTemplate restTemplate;
@@ -31,6 +31,10 @@ public class FortuneService {
 //
 //        ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
 //        return response.getBody();
+
+//        String endpoint = System.getenv("OPENAI_API_KEY");
+
+
         return apiKey;
     }
 }
