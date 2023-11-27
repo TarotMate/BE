fe10a60e714796024c44f98fd20bb232ab2dc134
46112 <duf325@naver.com>
2023-11-27 23:24:19 +0900

    config: 배포환경 aws-parameter값 비활성화

--- tarot/src/main/java/com/tarotmate/tarot/domain/fortune/application/FortuneService.java
+++ tarot/src/main/java/com/tarotmate/tarot/domain/fortune/application/FortuneService.java
@@ -31,6 +31,6 @@ public class FortuneService {
 //
 //        ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
 //        return response.getBody();
-        return apiKey;
+        return "success";
     }
 }
