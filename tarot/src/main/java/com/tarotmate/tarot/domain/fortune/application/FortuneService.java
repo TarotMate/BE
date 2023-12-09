d725c9f9adda0293e114948f4a90471a8003bdc6
46112 <duf325@naver.com>
2023-12-09 16:50:50 +0900

    feat: 오늘의 운세 기본 응답 추가

--- tarot/src/main/java/com/tarotmate/tarot/domain/fortune/application/FortuneService.java
+++ tarot/src/main/java/com/tarotmate/tarot/domain/fortune/application/FortuneService.java
@@ -1,14 +1,22 @@
 package com.tarotmate.tarot.domain.fortune.application;
 
+import com.tarotmate.tarot.global.errors.exception.ErrorCode;
+import com.tarotmate.tarot.global.errors.exception.Exception500;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -19,18 +27,32 @@ public class FortuneService {
 
     private final RestTemplate restTemplate;
 
-    public String getGptResponse(final String prompt) {
-//        String url = "https://api.openai.com/v1/engines/gpt-3.5-turbo-1106/completions";
-//
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_JSON);
-//        headers.setBearerAuth(apiKey);
-//
-//        String requestBody = "{\"prompt\": \"" + prompt + "\", \"max_tokens\": 150}";
-//        HttpEntity<String> request = new HttpEntity<>(requestBody, headers);
-//
-//        ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
-//        return response.getBody();
-        return "success";
+    public String getTodayFortune(final String card) {
+        if (Objects.equals(apiKey, "defaultValue")) throw new Exception500(ErrorCode.ER01);
+
+        try {
+            final String defaultUrl = "https://api.openai.com/v1/chat/completions";
+
+            final HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            headers.setBearerAuth(apiKey);
+
+            final Map<String, Object> body = new HashMap<>();
+            body.put("model", "gpt-3.5-turbo");
+            body.put("messages", List.of(
+                    Map.of("role", "system", "content", "You are a tarot reader. Your guests would like to know about their luck for the day. The guest draws only one card, and when it is presented to you, you must provide an interpretation of it. Your customer is Korean, so please answer in Korean."),
+                    Map.of("role", "user", "content", card)
+            ));
+            final HttpEntity<Map<String, Object>> request = new HttpEntity<>(body, headers);
+
+            final OpenAIResponseDTO response = restTemplate.postForObject(defaultUrl, request, OpenAIResponseDTO.class);
+
+            final OpenAIResponseDTO.Message message = Objects.requireNonNull(response).getChoices().get(0).getMessage();
+            final String content = Objects.requireNonNull(message).getContent();
+//.replace("\n", " ").replace("\\\"", "'")
+            return content;
+        } catch (final RestClientException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- tarot/src/main/java/com/tarotmate/tarot/domain/fortune/ui/FortuneController.java
+++ tarot/src/main/java/com/tarotmate/tarot/domain/fortune/ui/FortuneController.java
@@ -4,21 +4,22 @@ import com.tarotmate.tarot.domain.fortune.application.FortuneService;
 import com.tarotmate.tarot.global.utils.ApiResult;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class FortuneController {
 
     private final FortuneService fortuneService;
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/fortune")
     public ApiResult<String> readFortune() {
 
-        String result = fortuneService.getGptResponse("hello");
+        final String result = fortuneService.getTodayFortune("The Chariot");
         return ApiResult.success(result);
     }
 }
