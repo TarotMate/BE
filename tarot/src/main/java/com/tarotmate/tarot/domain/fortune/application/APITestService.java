68308f9845dd6188005745d4ec334a268b22cf0b
46112 <duf325@naver.com>
2023-12-30 18:20:54 +0900

    feat: API test를 위한 기본 get응답 api 생성

---
+++ tarot/src/main/java/com/tarotmate/tarot/domain/fortune/application/APITestService.java
@@ -0,0 +1,11 @@
+package com.tarotmate.tarot.domain.fortune.application;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class APITestService {
+
+    public String getDefaultResponse(){
+        return "DefaultResponse";
+    }
+}
