5877f7d76a5aaaa6a8fc91ad301d6896114b1576
46112 <duf325@naver.com>
2023-12-31 16:13:18 +0900

    chore: API teset controller 로깅 추가

--- tarot/src/main/java/com/tarotmate/tarot/domain/fortune/ui/APITestController.java
+++ tarot/src/main/java/com/tarotmate/tarot/domain/fortune/ui/APITestController.java
@@ -3,17 +3,22 @@ package com.tarotmate.tarot.domain.fortune.ui;
 import com.tarotmate.tarot.domain.fortune.application.APITestService;
 import com.tarotmate.tarot.domain.fortune.application.FortuneResponseDTO;
 import com.tarotmate.tarot.global.utils.ApiResult;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class APITestController {
 
     private final APITestService apiTestService;
+
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/api-test")
