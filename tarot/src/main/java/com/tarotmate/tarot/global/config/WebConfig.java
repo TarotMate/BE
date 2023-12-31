4e145ed1b2131d119bc73090a1e1c4f64aecb808
46112 <duf325@naver.com>
2023-12-31 17:58:05 +0900

    feat: cors설정 www 서브도메인 허용

--- tarot/src/main/java/com/tarotmate/tarot/global/config/WebConfig.java
+++ tarot/src/main/java/com/tarotmate/tarot/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 설정 적용
-                .allowedOrigins("https://tarotmate.kr",  "http://localhost:5173") // 여기서는 tarotmate.kr 도메인에서의 요청만 허용
+                .allowedOrigins("https://tarotmate.kr", "https://www.tarotmate.kr",  "http://localhost:5173") // 여기서는 tarotmate.kr 도메인에서의 요청만 허용
                 .allowedMethods("GET", "POST") // 허용할 HTTP 메소드 명시
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true) // 인증 정보 허용 (예: 쿠키, HTTP 인증)
