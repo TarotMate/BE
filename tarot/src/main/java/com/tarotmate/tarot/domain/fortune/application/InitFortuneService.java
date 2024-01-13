46739e3b81d193906cfad9c7b0a835f3f9ed438d
46112 <duf325@naver.com>
2024-01-13 16:38:17 +0900

    delete: 불필요한 테스트 api 제거

--- tarot/src/main/java/com/tarotmate/tarot/domain/fortune/application/InitFortuneService.java
+++ tarot/src/main/java/com/tarotmate/tarot/domain/fortune/application/InitFortuneService.java
@@ -1,6 +1,7 @@
 package com.tarotmate.tarot.domain.fortune.application;
 
 import com.tarotmate.tarot.domain.fortune.application.model.InitialPageResponse;
+import com.tarotmate.tarot.global.errors.exception.ErrorCode;
 import com.tarotmate.tarot.global.errors.exception.Exception500;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -22,73 +23,9 @@ public class InitFortuneService {
         final InitialPageResponse initialPageResponse;
         try {
             initialPageResponse = initialPageLoader.loadInitialData();
-        } catch (IOException e) {
-            throw new Exception500("실패");
+        } catch (final IOException e) {
+            throw new Exception500(ErrorCode.ER10);
         }
         return initialPageResponse;
     }
-
-
-/*
-    public TarotPageResponse getTarotPage(String fortuneType, String theme){
-        final TarotPageResponse tarotPage = TarotPageResponse.getDefaultTarotPage();
-
-        if (Objects.equals("today-tarot", fortuneType)) {
-            switch (theme) {
-                case "star-story":
-                    break;
-                case "today-preview":
-                    tarotPage.changeTheme("오늘의 에너지", Arrays.asList("오늘의 에너지", "기회와 도전", "중요한 선택"));
-                    break;
-                case "daily-insight":
-                    tarotPage.changeTheme("일상의 작은 기쁨", Arrays.asList("일상의 작은 기쁨", "주의깊게 살펴볼 것", "숨겨진 기회"));
-                    break;
-                case "end-of-day":
-                    tarotPage.changeTheme("오늘의 교훈", Arrays.asList("오늘의 교훈", "감사할 점", "내일을 위한 준비"));
-                    break;
-                default:
-                    throw new Exception400(ErrorCode.ER06);
-            }
-        }
-        else if (Objects.equals("love-tarot", fortuneType)) {
-            switch (theme) {
-                case "star-story":
-                    tarotPage.changeFortuneType(InitPage.LOVE_TAROT, "심장의 메시지", Arrays.asList("심장의 메시지", "새로운 인연", "애정의 조화와 갈등"));
-                    break;
-                case "new-love":
-                    tarotPage.changeFortuneType(InitPage.LOVE_TAROT, "첫 만남의 운", Arrays.asList("첫 만남의 운", "새로운 감정의 발견", "인연의 가능성"));
-                    break;
-                case "relationship":
-                    tarotPage.changeFortuneType(InitPage.LOVE_TAROT, "감정의 깊이", Arrays.asList("감정의 깊이", "서로의 이해", "관계의 진전"));
-                    break;
-                case "recovery":
-                    tarotPage.changeFortuneType(InitPage.LOVE_TAROT, "내면의 치유", Arrays.asList("내면의 치유", "새로운 시작", "자기 사랑"));
-                    break;
-                default:
-                    throw new Exception400(ErrorCode.ER06);
-            }
-        }
-        else if (Objects.equals("this-month-tarot", fortuneType)) {
-            switch (theme) {
-                case "star-story":
-                    tarotPage.changeFortuneType(InitPage.TAROT_OF_THE_MONTH, "심장의 메시지", Arrays.asList("심장의 메시지", "새로운 인연", "애정의 조화와 갈등"));
-                    break;
-                case "new-love":
-                    tarotPage.changeFortuneType(InitPage.TAROT_OF_THE_MONTH, "첫 만남의 운", Arrays.asList("첫 만남의 운", "새로운 감정의 발견", "인연의 가능성"));
-                    break;
-                case "relationship":
-                    tarotPage.changeFortuneType(InitPage.TAROT_OF_THE_MONTH, "감정의 깊이", Arrays.asList("감정의 깊이", "서로의 이해", "관계의 진전"));
-                    break;
-                case "recovery":
-                    tarotPage.changeFortuneType(InitPage.TAROT_OF_THE_MONTH, "내면의 치유", Arrays.asList("내면의 치유", "새로운 시작", "자기 사랑"));
-                    break;
-                default:
-                    throw new Exception400(ErrorCode.ER06);
-            }
-        }
-        else throw new Exception400(ErrorCode.ER07);
-
-        return tarotPage;
-    }
- */
 }
--- tarot/src/main/java/com/tarotmate/tarot/domain/fortune/ui/FortuneController.java
+++ tarot/src/main/java/com/tarotmate/tarot/domain/fortune/ui/FortuneController.java
@@ -1,14 +1,11 @@
 package com.tarotmate.tarot.domain.fortune.ui;
 
-import com.tarotmate.tarot.domain.fortune.application.APITestService;
 import com.tarotmate.tarot.domain.fortune.application.ReadFortuneService;
 import com.tarotmate.tarot.domain.fortune.application.InitFortuneService;
 import com.tarotmate.tarot.domain.fortune.application.model.FortuneResponse;
-import com.tarotmate.tarot.domain.fortune.application.model.FortuneResponseDTO;
 import com.tarotmate.tarot.domain.fortune.application.model.InitialPageResponse;
 import com.tarotmate.tarot.domain.fortune.application.model.TarotRequest;
 import com.tarotmate.tarot.global.utils.ApiResult;
-import com.tarotmate.tarot.global.utils.prompt.FortunePrompt;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,14 +20,6 @@ public class FortuneController {
 
     private final InitFortuneService initFortuneService;
     private final ReadFortuneService readFortuneService;
-    private final APITestService apiTestService;
-
-    @ResponseStatus(HttpStatus.OK)
-    @GetMapping("/api-test")
-    public ApiResult<FortuneResponseDTO> returnDefaultResponse() {
-        final FortuneResponseDTO result = apiTestService.getDefaultResponse();
-        return ApiResult.success(result);
-    }
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/init")
@@ -38,15 +27,7 @@ public class FortuneController {
 
         return ApiResult.success(initFortuneService.getTarotPage());
     }
-/*
-    @ResponseStatus(HttpStatus.OK)
-    @GetMapping("/init2")
-    public TarotPageResponse getHomePage(@RequestParam(required = false, defaultValue = "today-tarot") final String fortuneType,
-                                          @RequestParam(required = false, defaultValue = "star-story") final String theme) {
 
-        return initFortuneService.getTarotPage(fortuneType, theme);
-    }
-*/
     @ResponseStatus(HttpStatus.OK)
     @PostMapping("/tarot")
     public ApiResult<FortuneResponse> readFortune(@RequestBody final TarotRequest tarotRequest) {
@@ -54,10 +35,5 @@ public class FortuneController {
         final FortuneResponse result = readFortuneService.getTarotResult(tarotRequest);
 
         return ApiResult.success(result);
-    }
-
-    @GetMapping("/test")
-    public String test() {
-        return FortunePrompt.getReturnTypeSetting();
     }
 }
