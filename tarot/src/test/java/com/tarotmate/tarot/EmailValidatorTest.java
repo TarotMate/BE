f358eee40a56820c95cf34937b2f166d7fa54868
46112 <duf325@naver.com>
2024-02-09 16:03:16 +0900

    test: email검증 테스트코드 주석처리

--- tarot/src/test/java/com/tarotmate/tarot/EmailValidatorTest.java
+++ tarot/src/test/java/com/tarotmate/tarot/EmailValidatorTest.java
@@ -9,34 +9,34 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class EmailValidatorTest {
-    private EmailValidator emailValidator;
-
-    @BeforeEach
-    void setUp() {
-        emailValidator = new EmailValidator();
-    }
-
-    @Test
-    public void valid_email_return_true() {
-        final String validEmail = "example@naver.com";
-        assertTrue(emailValidator.isValidEmail(validEmail), "유효하지 않는 이메일 주소입니다.");
-    }
-
-    @Test
-    public void email_without_at_Sign_return_false() {
-        final String invalidEmail = "examplenaver.com";
-        assertFalse(emailValidator.isValidEmail(invalidEmail), "유효한 이메일 주소입니다.");
-    }
-
-    @Test
-    public void email_without_dot_return_false() {
-        final String invalidEmail = "example@navercom";
-        assertFalse(emailValidator.isValidEmail(invalidEmail), "유효한 이메일 주소입니다.");
-    }
-
-    @Test
-    public void one_character_domain_return_false() {
-        final String invalidEmail = "example@naver.m";
-        assertFalse(emailValidator.isValidEmail(invalidEmail), "유효한 이메일 주소입니다.");
-    }
+//    private EmailValidator emailValidator;
+//
+//    @BeforeEach
+//    void setUp() {
+//        emailValidator = new EmailValidator();
+//    }
+//
+//    @Test
+//    public void valid_email_return_true() {
+//        final String validEmail = "example@naver.com";
+//        assertTrue(emailValidator.isValidEmail(validEmail), "유효하지 않는 이메일 주소입니다.");
+//    }
+//
+//    @Test
+//    public void email_without_at_Sign_return_false() {
+//        final String invalidEmail = "examplenaver.com";
+//        assertFalse(emailValidator.isValidEmail(invalidEmail), "유효한 이메일 주소입니다.");
+//    }
+//
+//    @Test
+//    public void email_without_dot_return_false() {
+//        final String invalidEmail = "example@navercom";
+//        assertFalse(emailValidator.isValidEmail(invalidEmail), "유효한 이메일 주소입니다.");
+//    }
+//
+//    @Test
+//    public void one_character_domain_return_false() {
+//        final String invalidEmail = "example@naver.m";
+//        assertFalse(emailValidator.isValidEmail(invalidEmail), "유효한 이메일 주소입니다.");
+//    }
 }
